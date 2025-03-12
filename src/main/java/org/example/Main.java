497a33cc93d25aa2e843336494cad642e0fd6a4a
diego <622473@alu.murciaeduca.es>
2025-03-12 20:49:16 +0100

    Creamos el método initAgenda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Crear una nueva agendaContactos.
-        Agenda agendaContactos = new Agenda();
-
-        // Añadir contactos a la agendaContactos.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        Agenda agendaContactos = initAgenda();
 
         // Imprimir todos los contactos de la agendaContactos.
         System.out.println("Todos los contactos: ");
@@ -29,5 +24,15 @@ public class Main {
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+
+    private static Agenda initAgenda() {
+        Agenda agendaContactos = new Agenda();
+
+        // Añadir contactos a la agendaContactos.
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
+        return agendaContactos;
     }
 }
